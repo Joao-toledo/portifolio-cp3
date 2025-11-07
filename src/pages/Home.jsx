d16b9206f452ef2676e9f19ce13bbcafca7ad771
Joao toledo <jvmstoledo@gmail.com>
2025-11-07 09:57:04 -0300

    feat: contato e footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { Contato } from "../componets/contato";
 import { Estrela } from "../componets/Estrela";
 import { Habilidades } from "../componets/Habilidades";
 import { Inicio } from "../componets/Inicio";
 import { Navbar } from "../componets/NavBar";
 import { Projetos } from "../componets/Projetos";
+import { Rodape } from "../componets/Rodape";
 import { SobreMim } from "../componets/SobreMim";
 import { Tema } from "../componets/Tema";
 
@@ -18,8 +20,9 @@ export const Home = () => {
                 <SobreMim />
                 <Habilidades />
                 <Projetos />
-                
+                <Contato />
             </main>
+            <Rodape />
             
         </div>
     )
