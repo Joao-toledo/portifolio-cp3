666e97108daff6437be9d1a58aa4d3db44e31a02
Joao toledo <jvmstoledo@gmail.com>
2025-11-07 07:05:34 -0300

    feat: background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { Estrela } from "../componets/Estrela";
 import { Tema } from "../componets/Tema";
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <Tema/>
+            <Estrela />
+            
         </div>
     )
 
